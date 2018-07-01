466dfd2190a6b80e231463305ecb840b5a25a0e9
Alexandru-Marian BRĂTAN <alexbratan@hotmail.com>
2018-07-01 05:21:22 +0300

    First version of the validations. What need to be done: - diff than 0 - check input values for RPM - anything you find :))

--- Machine_Element_V2Scheme/MainWindow.xaml.cs
+++ Machine_Element_V2Scheme/MainWindow.xaml.cs
@@ -20,9 +20,70 @@ namespace Machine_Element_V2Scheme
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+
+            
+
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int variableRotationSpeed;
+            float variablePowerWorking, variableTotalRatio, variableMechanicalEfficiencyTct, variableMechanicalEfficiencyAk, variableMechanicalEfficiencyPr;
+
+
+            if (rotationSpeed.Text != "" && powerWorkingMachine.Text != "" && totalRatio.Text != "" && rotationSpeed.Text != "" && rotationSpeed.Text != "" && rotationSpeed.Text != "")
+            {
+                variableRotationSpeed = Int32.Parse(rotationSpeed.Text);
+                variablePowerWorking = float.Parse(powerWorkingMachine.Text);
+                variableTotalRatio = float.Parse(totalRatio.Text);
+                variableMechanicalEfficiencyTct = float.Parse(mechanicalEfficiencyTct.Text);
+                variableMechanicalEfficiencyAk = float.Parse(mechanicalEfficiencyAk.Text);
+                variableMechanicalEfficiencyPr = float.Parse(mechanicalEfficiencyPr.Text);
+                Buton.IsEnabled = false;
+                outputTemp.Content = variableRotationSpeed;
+            }
+            else
+                MessageBox.Show("You must enter a value!");
+            
+        }
+
+        private void rotationSpeed_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsValid_integer(((TextBox)sender).Text + e.Text);
+        }
+
+        public static bool IsValid_integer(string str)
+        {
+            int i;
+            return int.TryParse(str, out i);
+        }
+
+        private void powerWorkingMachine_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsValid_float(((TextBox)sender).Text + e.Text);
+        }
+
+        public static bool IsValid_float(string str)
+        {
+            float i;
+            return float.TryParse(str, out i);
+        }
+
+        private void mechanicalEfficiency_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsValid_mechanicalEfficiency(((TextBox)sender).Text + e.Text);
+        }
+
+        public static bool IsValid_mechanicalEfficiency(string str)
+        {
+            float i;
+            return float.TryParse(str, out i) && i <= 1;
+        }
+
+
     }
 }
